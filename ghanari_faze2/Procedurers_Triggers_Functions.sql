d9d72d4a06a0e474094d68ce1c177e49499b9778
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2021-07-12 15:43:50 +0430

    Update Procedurers_Triggers_Functions.sql

--- ghanari_faze2/Procedurers_Triggers_Functions.sql
+++ ghanari_faze2/Procedurers_Triggers_Functions.sql
@@ -261,5 +261,179 @@ DELIMITER ;
 
 
 #---------------------------------------FindNumberOfLikes---------------------------------------#
+# in this query users can find number of likes for each ava except ava's whose sender blocked him (دریافت تعدا پسند ها)
+DELIMITER $$
+CREATE PROCEDURE `FindNumberOfLikes`(
+	in id_ava int
+)
+BEGIN
+	select ava.Content as ava, count(avalike.WhoLiked) as num_likes
+	from (avalike inner join ava on ava.id = avalike.Ava) inner join block on ava.Sender = block.Blocker
+	where ava.id = id_ava and block.Blocked != GetCurrentUser()
+	group by ava.Content;
+END$$
+DELIMITER ;
 
 
+#---------------------------------------ListOfLikers---------------------------------------#
+# users can see who have liked a specific ava but by two conditions
+# one: this user can not see this list for users who blocked him
+# two: this user can not see for example user A liked this ava if A has blocked this user (دریافت لیست پسند کنندگان)
+DELIMITER $$
+CREATE PROCEDURE `ListOfLikers`(
+	in id_ava int
+)
+BEGIN
+	select ava.Content, avalike.WhoLiked
+	from (ava left join avalike on ava.id = avalike.Ava)
+	where ava.id = id_ava and ava.id not in (select distinct ava.id
+					 from ((ava left join avalike on ava.id = avalike.Ava)
+					 left join (block as b1) on b1.Blocker = ava.Sender)
+					 left join (block as b2) on b2.Blocker = avalike.WhoLiked
+					 where (b2.Blocked = GetCurrentUser() or b1.Blocked = GetCurrentUser()));
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindTrendAvas---------------------------------------#
+# in this query users can see avas which their 
+# sender has not blocked him ordered by likes (دریافت آواهای پرطرفدار)
+DELIMITER $$
+CREATE PROCEDURE `FindTrendAvas`()
+BEGIN
+	select ava.Content as ava_content, count(ava.id) as num_like
+	from (ava left join avalike on ava.id = avalike.Ava) inner join block on ava.Sender = block.Blocker
+	where block.Blocked != GetCurrentUser()
+	group by content
+	order by num_like desc;
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindCommentsOfAva---------------------------------------#
+# in this query users can find comments on a specific ava with two conditions
+# one: sender of top ava has not blocked him
+# two: sender of comments has not blocked him (دریافت نظرات آوا)
+DELIMITER $$
+CREATE PROCEDURE `FindCommentsOfAva`(
+	in id_ava int
+)
+BEGIN
+	select ava.Content, ava.Sender
+	from ava
+	where ava.CommentOn = id_ava 
+		and ava.id not in (
+			select ava.id
+			from ava left join block on ava.Sender = block.Blocker
+			where ava.CommentOn = id_ava and block.Blocked = GetCurrentUser())
+		and GetCurrentUser() not in(
+			select block.Blocked
+			from ava inner join block on ava.Sender = block.Blocker
+			where ava.id = id_ava);
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindAvaOfBeFollowed---------------------------------------#
+# users can find ava of users that follow them but not users that blocked him (دریافت فعالیت دنبال شوندگان)
+DELIMITER $$
+CREATE PROCEDURE `FindAvaOfBeFollowed`()
+BEGIN
+	select ava.Content, ava.Sender, ava.SendingTime
+	from ava inner join follow on ava.Sender = follow.ToBeFollowed
+	where follow.Follower = GetCurrentUser()
+	and GetCurrentUser() not in(
+		select block.Blocked
+		from block
+		where block.Blocker = ava.Sender)
+	order by ava.SendingTime desc;
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindAvaOfUsers---------------------------------------#
+# users can find ava of users who not blocked him (دریافت فعالیت کاربران)
+DELIMITER $$
+CREATE PROCEDURE `FindAvaOfUsers`(
+	in ava_sender varchar(20)
+)
+BEGIN
+	select ava.Content, ava.SendingTime
+	from ava 
+	where ava.Sender = ava_sender
+	and GetCurrentUser() not in(
+		select block.Blocked
+		from block
+		where block.Blocker = ava.Sender)
+	order by ava.SendingTime desc;
+END$$
+DELIMITER ;
+
+
+##################################################################################################
+#---------------------------------------SendMessage---------------------------------------#
+# users can send message to others with two conditions
+# one: he has not been blocked by receiver
+# two: he has not been blocked by sender of main ava of message (ارسال پیام)
+
+DELIMITER $$
+CREATE PROCEDURE `SendMessage`(
+	in Content_Ava int,
+    in Content_Text varchar(256),
+    in Receiver varchar(20)    
+)
+BEGIN
+	if (Content_Ava is null) and (Content_Text is not null) then
+        insert into message (ContentAva, ContentText, Sender, Receiver, SendingTime)
+		select Content_Ava, Content_Text, GetCurrentUser(), Receiver, current_timestamp()
+		where GetCurrentUser() not in(
+			select block.Blocked
+			from block
+			where block.Blocker = Receiver);
+	end if;
+    if (Content_Text is null) and (Content_Ava is not null) then
+		insert into message (ContentAva, ContentText, Sender, Receiver, SendingTime)
+		select Content_Ava , Content_Text, GetCurrentUser(), Receiver, current_timestamp()
+		where not exists(
+			select *
+			from ava left join block on ava.Sender = block.Blocker 
+			where block.Blocked = GetCurrentUser() and ava.id = Content_Ava);
+	end if;
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindReceivedMessages---------------------------------------#
+# users can find the messages been send to them
+# but can not see messages with ava content that 
+# main sender of ava blocked them (دریافت لیست پیام های دریافتی کاربر)
+DELIMITER $$
+CREATE PROCEDURE `FindReceivedMessages`()
+BEGIN
+	select *
+	from message
+	where message.Receiver = GetCurrentUser() 
+    and not exists(
+		select block.Blocked
+		from ava left join block on ava.Sender = block.Blocker
+		where message.ContentAva = ava.id and block.Blocked = GetCurrentUser())
+	order by message.SendingTime desc;
+END$$
+DELIMITER ;
+
+
+#---------------------------------------FindValidMessageSendersToMe---------------------------------------#
+# users can see who has sent to him valid messages (دریافت لیست ارسال کنندگان پیام)
+DELIMITER $$
+CREATE PROCEDURE `FindValidMessageSendersToMe`()
+BEGIN
+	select distinct message.Sender
+	from message
+	where message.Receiver = GetCurrentUser()
+    and not exists(
+		select block.Blocked
+		from ava left join block on ava.Sender = block.Blocker
+		where message.ContentAva = ava.id and block.Blocked = GetCurrentUser())
+	order by message.SendingTime desc;
+END$$
+DELIMITER ;
